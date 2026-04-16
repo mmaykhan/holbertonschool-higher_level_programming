b01ed418d30d7030795c2dc3a86d6bf3d7b070d5
mmaykhan <mmmayxan10@gmail.com>
2026-04-16 06:12:11 +0000

    the answer oft ask 2

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6-raise_exception_msg
+Function that raises a NameError with a message
+"""
+
+
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError with a specific message.
+    """
+    raise NameError(message)
