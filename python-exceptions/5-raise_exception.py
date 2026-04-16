0133b6fdbd13b0487becb0cdbe3381e2e968d57b
mmaykhan <mmmayxan10@gmail.com>
2026-04-16 06:07:40 +0000

    tthe answer of taks 3

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-raise_exception
+Function that raises a TypeError
+"""
+
+
+def raise_exception():
+    """
+    Raises a TypeError exception qəsdən.
+    """
+    raise TypeError
