812bd0e966dc87ff799574f18b76fa1016c7f9ad
mmaykhan <mmmayxan10@gmail.com>
2026-04-16 16:34:31 +0000

    the first answer of task 2

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current database
+-- The table has columns id (INT) and name (VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
