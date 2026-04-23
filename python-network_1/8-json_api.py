694f80bd2fef595f2844ba2f0e7901cc57c6cc9a
mmaykhan <mmmayxan10@gmail.com>
2026-04-23 06:14:43 +0000

    the answer of task 8

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    payload = {"q": q}
+    try:
+        r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+        response = r.json()
+        if response:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
