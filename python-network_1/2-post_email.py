d9eb683ec89c2f49989d40fba8bdc07be574bf92
mmaykhan <mmmayxan10@gmail.com>
2026-04-23 05:50:59 +0000

    the asnwer of task 2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,8 +3,8 @@
 A script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter.
 """
-import urllib.request
 import urllib.parse
+import urllib.request
 import sys
 
 
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
-    
+
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
